18447ed9a8f2aae985c26cc59aaa724821561d60
mie2001 <450921468@qq.com>
2021-01-19 22:24:10 +0800

    master

---
+++ 1/2.c
@@ -0,0 +1,8 @@
+/* The first C programme */
+#include <stdio.h> /* 包含标准输入输出头文件 */
+int main()         /* 主函数 */
+{
+    printf("Hello World!\n"); /* 打印输出信息 */
+    system("pause");
+    return 0;
+}
