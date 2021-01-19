e3117e502839a69fb1be8c952e0973f2fecb2844
mie2001 <450921468@qq.com>
2021-01-19 21:35:56 +0800

    master

--- c-test.c
+++ c-test.c
@@ -3,7 +3,8 @@
 #include <time.h>
 int main()
 {
-    printf("This is a test for git!\n");
+    printf("This is a test for git! \n");
+    printf("This is a test for github! \n");
     system("pause");
     return 0;
 }
